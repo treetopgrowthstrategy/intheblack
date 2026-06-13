02aafe07420c787a439404b8a087b1645a681534
treetopgrowthstrategy <william.colbert@treetopgrowthstrategy.com>
2026-06-13 10:30:52 -0500

    add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/serverless';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://intheblack.coffee',
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   output: 'server',
   adapter: vercel(),
 });
